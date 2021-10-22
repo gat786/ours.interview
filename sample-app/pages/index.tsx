bbf815613abfd1a312f0bf208d8ecb6d0bced18c
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-22 18:19:11 +0530

    added material icons and completed first part

--- sample-app/pages/index.tsx
+++ sample-app/pages/index.tsx
@@ -17,15 +17,29 @@ const Home: NextPage = () => {
       <main>
         <Nav />
 
-        <div className="flex flex-col bg-pink-200 items-center justify-center">
+        <div className="flex flex-col gap-6 items-center justify-center mlb-16">
           <div className="relative w-60 h-80">
             <Image src="/ui/main-image.png" layout="fill"></Image>
           </div>
-          <h3 className="text-3xl font-bold nunito">Live a Yogic Lifestyle</h3>
-          <p>
-            A Daily Practice consisting of Yoga, Meditation, Chanting and
-            Relaxation Techniques, led by Traditional Indian Practitioners.
-          </p>
+          <div className="text-center w-80 flex flex-col gap-4">
+            <h3 className="text-3xl font-bold nunito">Live a Yogic Lifestyle</h3>
+            <p className="nunito text-sm">
+              A Daily Practice consisting of Yoga, Meditation, Chanting and
+              Relaxation Techniques, led by Traditional Indian Practitioners.
+            </p>
+          </div>
+          <button className="bg-primary p-4 rounded-xl text-white nunito">
+            Start your Journey
+          </button>
+        </div>
+
+        <div className="flex flex-col gap-8 items-center justify-center">
+          <i className="material-icons">expand_more</i>
+
+          <div className="flex gap-40">
+            <div className="h-1 bg-gray-200 w-40"></div>
+            <div className="h-1 bg-gray-200 w-40"></div>
+          </div>
         </div>
       </main>
     </div>
