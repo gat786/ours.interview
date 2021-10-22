3682c75712ba636be2c3eab18421d5706a8cfeb9
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-22 11:50:22 +0000

    added tailwindcss logical

--- sample-app/pages/index.tsx
+++ sample-app/pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -12,11 +13,24 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        
+      <main>
+        <nav className="flex items-center justify-between p-4">
+          <div className="flex items-center gap-2">
+            <Image src={"/logo.png"} height={32} width={32} />
+            <p className="uppercase">Ours</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Link href={""}>
+              <a className="text-primary py-3 px-4 rounded-xl">Log in</a>
+            </Link>
+            <Link href="">
+              <a className="bg-primary text-white py-3 px-4 rounded-xl">Sign up</a>
+            </Link>
+          </div>
+        </nav>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
