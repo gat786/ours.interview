9a2159c79397bfb55b39356b836fac51c1c4fb29
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-22 18:42:49 +0530

    created property cards

---
+++ sample-app/data/content.ts
@@ -0,0 +1,21 @@
+import { PropertyProps } from "../pages";
+
+const PropertiesOfYoga: PropertyProps[] = [
+  {
+    icon: "/ui/bhagvadgita.png",
+    title: "Traditional",
+    subtitle: "Designed by alumni of the oldest Yoga institutes in India.",
+  },
+  {
+    icon: "/ui/non-duality.png",
+    title: "Holistic",
+    subtitle: "A set of daily practices for your mind, body and spirit.",
+  },
+  {
+    icon: "/ui/compassion.png",
+    title: "For Beginners",
+    subtitle: "Master the basics, and unlock new techniques as you progress.",
+  },
+];
+
+export default { PropertiesOfYoga };
--- sample-app/pages/index.tsx
+++ sample-app/pages/index.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import Nav from "../components/Nav";
 import styles from "../styles/Home.module.css";
 
+import Content from "data/content";
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -22,7 +24,9 @@ const Home: NextPage = () => {
             <Image src="/ui/main-image.png" layout="fill"></Image>
           </div>
           <div className="text-center w-80 flex flex-col gap-4">
-            <h3 className="text-3xl font-bold nunito">Live a Yogic Lifestyle</h3>
+            <h3 className="text-3xl font-bold nunito">
+              Live a Yogic Lifestyle
+            </h3>
             <p className="nunito text-sm">
               A Daily Practice consisting of Yoga, Meditation, Chanting and
               Relaxation Techniques, led by Traditional Indian Practitioners.
@@ -36,12 +40,41 @@ const Home: NextPage = () => {
         <div className="flex flex-col gap-8 items-center justify-center">
           <i className="material-icons">expand_more</i>
 
-          <div className="flex gap-40">
-            <div className="h-1 bg-gray-200 w-40"></div>
-            <div className="h-1 bg-gray-200 w-40"></div>
+          <div className="flex gap-80">
+            <div className="bg-gray-200 w-96 h-0.5"></div>
+            <div className="h-0.5 bg-gray-200 w-96"></div>
           </div>
         </div>
+
+        <div className="flex justify-center gap-28">
+          {Content.PropertiesOfYoga.map((property, index) => (
+            <PropertyCard
+              key={index}
+              icon={property.icon}
+              title={property.title}
+              subtitle={property.subtitle}
+            />
+          ))}
+        </div>
       </main>
+    </div>
+  );
+};
+
+export interface PropertyProps {
+  icon: string;
+  title: string;
+  subtitle: string;
+}
+
+const PropertyCard = (props: PropertyProps) => {
+  return (
+    <div className="w-60 flex flex-col gap-4 items-center nunito text-center my-8">
+      <div className="relative h-12 w-12">
+        <Image src={props.icon} layout="fill" />
+      </div>
+      <p className="font-bold text-xl">{props.title}</p>
+      <p>{props.subtitle}</p>
     </div>
   );
 };
