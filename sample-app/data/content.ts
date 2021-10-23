0447844ed3c6042b9c105a45cf7af905bc7bebdb
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-23 17:42:26 +0530

    added sadhana section

--- sample-app/data/content.ts
+++ sample-app/data/content.ts
@@ -1,4 +1,5 @@
 import { PropertyProps } from "components/PropertyCard";
+import { SadhanaCardProps } from "pages";
 
 const PropertiesOfYoga: PropertyProps[] = [
   {
@@ -18,4 +19,50 @@ const PropertiesOfYoga: PropertyProps[] = [
   },
 ];
 
-export default { PropertiesOfYoga };
+const Sadhanas: SadhanaCardProps[] = [
+  {
+    title: "Haṭha Yoga",
+    description:
+      "Start your day with a combination of Postures (Āsana) and Breathwork (Prāṇāyāma) to balance the energy in your body and enhance meditation.",
+    leadBy: "Harshvardhan Jhaveri",
+    alumniOf: "Kaivalyadham Yoga Institute",
+    icon: "/ui/vrikshasan.png",
+    startTime: "7:00 AM",
+    minutes: 30,
+    cardBg: "#F4DFB0",
+  },
+  {
+    title: "Meditation",
+    description:
+      "A morning Meditation (Dhyāna) to calm the mind and detach from the senses. An ancient tecnique to help relieve stress and stay more present.",
+    leadBy: "Gunjan Sugandhi",
+    alumniOf: "The Yoga Institute",
+    icon: "/ui/padmasan.png",
+    startTime: "7:30 AM",
+    minutes: 10,
+    cardBg: "#ACD9EA",
+  },
+  {
+    title: "Mantra Chanting",
+    description:
+      "Concentration (Dhāraṇā) through the repetitive chanting of sacred words in Sanskrit helps release positive energy, which leads to lasting inner peace.",
+    artists: ["Anurag Dhoundeyal", "Nrtya", "Jahaan Shah", "Srishti Biyani"],
+    icon: "/ui/aum.png",
+    startTime: "6:00 PM",
+    minutes: 10,
+    cardBg: "#DD7C3A",
+  },
+  {
+    title: "Yoga Nidra",
+    description:
+      "End the day with Yoga Nidra, an ancient relaxation tool for achieving Withdrawal (Pratyāhāra). It is a state of consciousness between the waking and dreaming.",
+    leadBy: "Gunjan Sugandhi",
+    alumniOf: "The Yoga Institute",
+    icon: "/ui/shavasan.png",
+    startTime: "10:00 PM",
+    minutes: 10,
+    cardBg: "#364263",
+  },
+];
+
+export default { PropertiesOfYoga, Sadhanas };
