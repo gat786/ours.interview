d9aaa8eccc247efe018ee2d1d06cf96c5d3fc9fa
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-23 22:39:56 +0530

    added breakpoints for elements

--- sample-app/components/PropertyCard.tsx
+++ sample-app/components/PropertyCard.tsx
@@ -1,21 +1,21 @@
 import Image from "next/image";
 
 export interface PropertyProps {
-    icon: string;
-    title: string;
-    subtitle: string;
-  }
-  
-  const PropertyCard = (props: PropertyProps) => {
-    return (
-      <div className="w-60 flex flex-col gap-4 items-center nunito text-center my-8">
-        <div className="relative h-12 w-12">
-          <Image src={props.icon} layout="fill" />
-        </div>
-        <p className="font-bold text-xl">{props.title}</p>
-        <p className="text-gray-700">{props.subtitle}</p>
-      </div>
-    );
-  };
+  icon: string;
+  title: string;
+  subtitle: string;
+}
 
-  export default PropertyCard;
+const PropertyCard = (props: PropertyProps) => {
+  return (
+    <div className="w-60 justify-self-center flex flex-col gap-4 items-center nunito text-center my-8">
+      <div className="relative h-12 w-12">
+        <Image src={props.icon} layout="fill" />
+      </div>
+      <p className="font-bold text-xl">{props.title}</p>
+      <p className="text-gray-700">{props.subtitle}</p>
+    </div>
+  );
+};
+
+export default PropertyCard;
--- sample-app/pages/index.tsx
+++ sample-app/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
       <main>
         <Nav />
 
-        <div className="flex flex-col gap-6 items-center justify-center mlb-16">
+        <div className="flex flex-col gap-6 items-center justify-center mlb-32">
           <div className="relative w-60 h-80">
             <Image src="/ui/main-image.png" layout="fill"></Image>
           </div>
@@ -43,48 +43,47 @@ const Home: NextPage = () => {
           </button>
         </div>
 
-        <div className="flex flex-col gap-8 items-center justify-center">
-          <i className="material-icons">expand_more</i>
-
-          <div className="flex gap-80">
-            <div className="bg-gray-200 w-96 h-0.5"></div>
-            <div className="h-0.5 bg-gray-200 w-96"></div>
+        <section className="flex flex-col items-center w-full">
+          <div className="flex w-5/6 md:w-1/2 justify-center items-center gap-6">
+            <div className="bg-black w-20 md:w-1/4 h-px"></div>
+            <div className="nunito font-bold text-3xl">Why OURS?</div>
+            <div className="h-px bg-black w-20 md:w-1/4"></div>
           </div>
-        </div>
 
-        <div className="flex justify-center gap-28">
-          {Content.PropertiesOfYoga.map((property, index) => (
-            <PropertyCard
-              key={index}
-              icon={property.icon}
-              title={property.title}
-              subtitle={property.subtitle}
-            />
-          ))}
-        </div>
+          <div className="grid grid-cols-1 justify-self-center md:grid-cols-3 md:w-2/3 md:gap-8">
+            {Content.PropertiesOfYoga.map((property, index) => (
+              <PropertyCard
+                key={index}
+                icon={property.icon}
+                title={property.title}
+                subtitle={property.subtitle}
+              />
+            ))}
+          </div>
+        </section>
 
         <section className="bg-primary text-white flex flex-col gap-4 plb-12 items-center rounded-2xl">
-          <div className="w-1/2 flex flex-col items-center text-center gap-4 pbe-6 border-b border-white">
-            <p className="w-1/2 nunito text-3xl font-bold">
+          <div className="w-11/12 lg:w-1/2 flex flex-col items-center text-center gap-4 pbe-6 border-b border-white">
+            <p className="w-11/12 md:w-1/2 nunito text-3xl font-bold">
               “Yoga is the journey, of the self, through the self, to the self”
             </p>
             <p className="">Bhagavad Gītā, Verse 6.20</p>
           </div>
-          <div className="w-1/2 flex flex-col gap-6 items-center">
-            <p className="w-2/3">
+          <div className="w-11/12 lg:w-1/2 flex flex-col gap-6 items-center">
+            <p className="md:w-2/3">
               Yoga is the ancient Indian science of healthy living - harmonizing
               the relationship between mind, body and environment. Much more
               than a physical exercise, it is a disciplined and holistic
               lifestyle that includes of a variety of practices.
             </p>
-            <p className="w-2/3">
+            <p className="md:w-2/3">
               {" "}
               Traditional Yoga is a combination of Ethical Rules (Yamas),
               Personal Observances (Niyamas), Posture (Āsana), Breathing
               (Prāṇāyāma), Withdrawal (Pratyāhāra), Concentration (Dhāraṇā) and
               Meditation (Dhyāna).
             </p>{" "}
-            <p className="w-2/3">
+            <p className="md:w-2/3">
               {" "}
               Our Sādhanā (Daily Practice) is designed for you to incorporate
               this Yogic lifestyle through short lessons, which can be viewed at
@@ -122,22 +121,24 @@ const Home: NextPage = () => {
           </div>
         </section>
 
-        <div className="flex items-center gap-6 justify-center">
-          <div className="bg-gray-200 w-1/3 h-0.5"></div>
-          <p className="nunito font-bold text-3xl">Benefits</p>
-          <div className="h-0.5 bg-gray-200 w-1/3"></div>
-        </div>
+        <section className="grid grid-flow-row">
+          <div className="w-1/2 flex items-center justify-self-center gap-6 justify-center">
+            <div className="bg-gray-200 w-1/4 h-0.5"></div>
+            <p className="nunito font-bold text-3xl">Benefits</p>
+            <div className="h-0.5 bg-gray-200 w-1/4"></div>
+          </div>
 
-        <div className="flex justify-center gap-28">
-          {Content.Benefits.map((property, index) => (
-            <PropertyCard
-              key={index}
-              icon={property.icon}
-              title={property.title}
-              subtitle={property.subtitle}
-            />
-          ))}
-        </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 md:w-2/3 justify-self-center md:gap-8">
+            {Content.Benefits.map((property, index) => (
+              <PropertyCard
+                key={index}
+                icon={property.icon}
+                title={property.title}
+                subtitle={property.subtitle}
+              />
+            ))}
+          </div>
+        </section>
 
         <section className="bg-primary text-white flex flex-col plb-12 items-center rounded-2xl">
           <p className="nunito text-3xl mbe-9 font-bold">Manifesto</p>
@@ -199,10 +200,10 @@ const Home: NextPage = () => {
         </section>
 
         <section className="flex flex-col items-center">
-          <div className="flex items-center gap-6 justify-center">
-            <div className="bg-gray-800 w-1/3 h-0.5"></div>
+          <div className="w-1/2 flex items-center gap-6 justify-center">
+            <div className="bg-gray-200 w-1/4 h-0.5"></div>
             <p className="nunito font-bold text-3xl">FAQs</p>
-            <div className="h-0.5 bg-gray-800 w-1/3"></div>
+            <div className="h-0.5 bg-gray-200 w-1/4"></div>
           </div>
 
           <div className="w-1/2">
